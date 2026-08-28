441fb32e6cad7a1bad2f2db61390c2119c00783d
Meghana Bonthu <meghanareddybonthu16@gmail.com>
2026-08-28 14:35:56 -0500

    chore: upgrade frontend to Angular 21

--- src/opspilot-web/src/main.ts
+++ src/opspilot-web/src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, { providers: [provideHttpClient()] })
+bootstrapApplication(AppComponent, { providers: [provideZoneChangeDetection(),provideHttpClient()] })
   .catch(error => console.error(error));
